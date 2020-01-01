62c254181b29ac05d1a6522c31fbf5c75d3f4af7
prodes13 <ciobotaru.florin@gmail.com>
2020-01-01 22:31:22 +0200

    database solve

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        
         <!-- Styles -->
         <style>
             html, body {
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    FreeCodeCamp
                 </div>
 
                 <div class="links">

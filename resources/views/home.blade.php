79eb3e53cc262bb391c1b4270d3903bd8177b0f9
prodes13 <ciobotaru.florin@gmail.com>
2020-01-02 00:06:54 +0200

    profile page started

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fotp3-2.fna.fbcdn.net/v/t51.2885-19/s150x150/70985486_577637296311063_2240788552625422336_n.jpg?_nc_ht=instagram.fotp3-2.fna.fbcdn.net&_nc_ohc=xrPveZewv2IAX-EhVNY&oh=7d11a2c9eb64765a1cf3fd3fc5e57194&oe=5EAA2DD7" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>154</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.
+            </div>
+            <div><a href="https://www.freecodecamp.org/">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.fotp3-1.fna.fbcdn.net/v/t51.2885-15/e35/c133.0.483.483a/75281137_475638993059143_485622801232332313_n.jpg?_nc_ht=instagram.fotp3-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=U2htlN29cQoAX-xeLRp&oh=0c64f088ed3864518791484eded561aa&oe=5EA7CFE0">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.fotp3-1.fna.fbcdn.net/v/t51.2885-15/e35/c96.0.557.557a/71878998_3374967282521159_5951849523171776240_n.jpg?_nc_ht=instagram.fotp3-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=G2QohfyWc4AAX8xFere&oh=8e808fbe52e2e1f4d6e934a87fbf35ab&oe=5EA1360A">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.fotp3-3.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c1.0.748.748a/s640x640/75379747_1984863518283661_4296065131383461825_n.jpg?_nc_ht=instagram.fotp3-3.fna.fbcdn.net&_nc_cat=101&_nc_ohc=KrQfo81pgCgAX_Y4qLI&oh=8efcf62bc7d29be159888ddd0cdc1aed&oe=5E9B8B6E">
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'InstaFCC') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/logo.svg" alt="Logo" style="height: 20px; border-right: 1px solid #333;" class="pr-3"></div>
+                    <div class="pl-3">InstaFCC</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -51,7 +52,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->username }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">

eb0de625477ca7fcc0d2d41f82ca7450c43e98e9
prodes13 <ciobotaru.florin@gmail.com>
2020-01-02 23:24:26 +0200

    added default profile image

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $guarded = [];
+    
+    public function profileImage()
+    {
+        $imagePath = $this->image ? $this->image : 'profile/EIaCQsJiDDNXM6WIA9hfW49dH6NhiapFBdHFHapk.jpeg';
+        return '/storage/'.$imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,13 +7,23 @@
             <img src="/storage/{{ $post->image }}" class="w-100">
         </div>
         <div class="col-4">
-            <div>
-                <h3>{{ $post->user->username }}</h3>
-                <p>
-                    {{ $post->caption }}
-                </p>
+            <div class="d-flex align-items-center">
+                <div class="pr-3">
+                    <img src="{{ $post->user->profile->image }}" class="rounded-circle w-100" style="max-width: 40px;" alt="">
+                </div>
+                <div>
+                    <div class="font-weight-bold">
+                        <a href="/profile/{{ $post->user->id }}"><span class="font-weight-bold text-dark">{{ $post->user->username }}</span></a>
+                        <a href="#" class="pl-3">Follow</a>
+                    </div>
+                </div>      
             </div>
+            <hr>
+            <p><a href="/profile/{{ $post->user->id }}"><span class="font-weight-bold text-dark">{{ $post->user->username }}</span></a> {{ $post->caption }}</p>
         </div>
     </div>
 </div>
 @endsection
+
+
+
